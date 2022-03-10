967931df7cdac4a709ea5b2b880fc0ab551694c8
Dima Karpuk <karpukdima91.@mail.ru>
2022-03-10 22:12:35 +0300

    HelloAutocode

---
+++ maven-demo/src/main/java/com/epam/training/student_Dzmitry_Karpuk/task1/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.epam.training.student_Dzmitry_Karpuk.task1;
+
+public class HelloAutocode {static public void main(String[] args) {
+    System.out.println("Hello, Autocode!");
+ }
+}
+
+
+
